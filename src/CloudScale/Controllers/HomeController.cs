dc508f11981c8397067009f4bcbde7ed22b336ff
Javier Lozano <javier@lozanotek.com>
2012-10-27 09:50:53 -0500

    Minor changes...

--- src/CloudScale/Controllers/HomeController.cs
+++ src/CloudScale/Controllers/HomeController.cs
@@ -1,10 +1,22 @@
 ﻿using System.Web.Mvc;
 
 namespace CloudScale.Controllers {
+	using System.Web.Security;
+
 	public class HomeController : Controller {
 		public ActionResult Index() {
-
 			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Auth(string username, string password) {
+			var result = FormsAuthentication.Authenticate(username, password);
+
+			if(result) {
+				FormsAuthentication.SetAuthCookie(username, false);
+			}
+
+			return Index();
 		}
 
 		public ActionResult Blitz() {
